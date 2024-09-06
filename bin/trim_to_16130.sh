86c5cf7c8b89a645a47d89ae44f100e3f6e00427
Michelle Erkenberg <michelle_erkenberg@kickseed.localdomain>
2024-09-06 11:33:41 +0200

    trimmed seq and trim skript 2.0

--- bin/trim_to_16130.sh
+++ bin/trim_to_16130.sh
@@ -3,12 +3,26 @@
 # Set the desired length
 desired_length=16130
 
+# Set the number of bases per line (adjust this to match your original files)
+bases_per_line=60
+
 # create a 'trim' directory if it doesn't exist
 mkdir -p trim
 
 # Function to get the actual sequence name from the FASTA file
 get_seq_name() {
     head -n 1 "$1" | sed 's/^>//' | awk '{print $1}'
+}
+
+
+# Function to format FASTA sequence
+format_fasta() {
+    awk -v l=$bases_per_line '
+    NR==1 {print; next}
+    {
+        for (i=1; i<=length($0); i+=l)
+            print substr($0, i, l)
+    }' "$1" > "$2"
 }
 
 # For each .fas file in the current directory (mask)
@@ -25,8 +39,11 @@ for file in *.fas; do
         echo -e "${seq_name}\t0\t${desired_length}" > "${base_name}.bed"
         
         # Use bedtools to trim the sequence
-        bedtools getfasta -fi "$file" -bed "${base_name}.bed" -fo "trim/${base_name}.trimmed.fas"
+        bedtools getfasta -fi "$file" -bed "${base_name}.bed" -fo "trim/${base_name}.temp.fas"
         
+	 # Format the trimmed FASTA file
+        format_fasta "trim/${base_name}.temp.fas" "trim/${base_name}.trimmed.fas"
+	
         # Remove the temporary .bed file
         rm "${base_name}.bed"
         
