5eeb65f7f49758a72e913048aae4091fd0eebe90
Michelle Erkenberg <michelle_erkenberg@kickseed.localdomain>
2024-09-09 13:49:56 +0200

    corrected tree and script

--- bin/msa_aln_cutoff.sh
+++ bin/msa_aln_cutoff.sh
@@ -16,6 +16,6 @@ echo "Script executed successfully. The alignment has been saved in 'msa/combine
 cat msa/combined_cutoff.fasta /mnt/expressions/michelle_erkenberg/github/mpi_dogs/data/science_dogs/Canis_latrans.fasta /mnt/expressions/michelle_erkenberg/github/mpi_dogs/data/science_dogs/science_dogs_all.with_haps.fasta > msa/combined_cutoff_with_pub.fasta
 
 # create a msa using MAFFT with previously published dogs
-mafft msa/combined_cutoff_with_pub.fasta > msa/combined_cutoFf_with_pub.aln
+mafft msa/combined_cutoff_with_pub.fasta > msa/combined_cutoff_with_pub.aln
 
 echo "Script executed successfully. The alignment has been saved in 'msa/combined_cutoff_with_pub.aln'."
