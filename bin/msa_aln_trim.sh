2f53f6eb0ee9250f0f6ee991b9d7e84e8b3e1915
Michelle Erkenberg <michelle_erkenberg@kickseed.localdomain>
2024-09-09 13:19:17 +0200

    corrected tree and scripts

--- bin/msa_aln_trim.sh
+++ bin/msa_aln_trim.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
+# Set the working directory
+work_dir="/mnt/expressions/michelle_erkenberg/github/mpi_dogs/data/dog_samples/Consensus/mask/trim"
+msa_dir="/mnt/expressions/michelle_erkenberg/github/mpi_dogs/data/dog_samples/Consensus/msa"
+
+# Change to the working directory
+cd "$work_dir" || exit 1
+
+# Ensure the MSA directory exists
+mkdir -p "$msa_dir"
 
 # Combine all consensus files into one file in the 'msa' directory
-cat ../Consensus/mask/trim/*fas > msa/combined_trim.fasta
+cat *.fas > "$msa_dir/combined_trim.fasta"
 
 # Display all header lines of the combined fasta file
-grep '>' msa/combined_trim.fasta
+grep '>' "$msa_dir/combined_trim.fasta"
 
 # Create a multiple sequence alignment using MAFFT
-mafft msa/combined_trim.fasta > msa/combined_trim.aln
+mafft "$msa_dir/combined_trim.fasta" > "$msa_dir/combined_trim.aln"
 
-echo "Script executed successfully. The alignment has been saved in 'msa/combined_trim.aln'."
+echo "First part executed successfully. The alignment has been saved in '$msa_dir/combined_trim.aln'."
 
 # INCLUDING PREVIOUSLY PUBLISHED DOGS
 
-cat msa/combined_trim.fasta /mnt/expressions/michelle_erkenberg/github/mpi_dogs/data/science_dogs/Canis_latrans.fasta /mnt/expressions/michelle_erkenberg/github/mpi_dogs/data/science_dogs/science_dogs_all.with_haps.fasta > msa/combined_trim_with_pub.fasta
+cat "$msa_dir/combined_trim.fasta" \
+    /mnt/expressions/michelle_erkenberg/github/mpi_dogs/data/science_dogs/Canis_latrans.fasta \
+    /mnt/expressions/michelle_erkenberg/github/mpi_dogs/data/science_dogs/science_dogs_all.fasta \
+    > "$msa_dir/combined_trim_with_pub.fasta"
 
 # create a msa using MAFFT with previously published dogs
-mafft msa/combined_trim_with_pub.fasta > msa/combined_trim_with_pub.aln
+mafft "$msa_dir/combined_trim_with_pub.fasta" > "$msa_dir/combined_trim_with_pub.aln"
 
-echo "Script executed successfully. The alignment has been saved in 'msa/combined_trim_with_pub.aln'."
+echo "Script executed successfully. The alignment has been saved in '$msa_dir/combined_trim_with_pub.aln'."
--- bin/tree.sh
+++ bin/tree.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
 
-# Create phylogenetic tree with MPI dogs (org)
-FastTree -nt < combined.aln > mpi_dogs_tree.nwk
+# Set working directory
+WORKDIR="/mnt/expressions/michelle_erkenberg/github/mpi_dogs/data/dog_samples/Consensus/msa"
 
-echo "Phylogenetic tree for MPI dogs created and saved as 'msa/mpi_dogs_tree.nwk'."
+# Set output directory
+OUTDIR="/mnt/expressions/michelle_erkenberg/github/mpi_dogs/data/dog_samples/Consensus/msa"
+
+# Change to working directory
+cd "$WORKDIR"
+
+# Create phylogenetic tree with MPI dogs (org)
+FastTree -nt < combined.aln > "$OUTDIR/mpi_dogs_tree.nwk"
+
+echo "Phylogenetic tree for MPI dogs created and saved as '$OUTDIR/mpi_dogs_tree.nwk'."
 
 # Create phylogenetic tree with MPI dogs and published dogs
-FastTree -nt < combined_with_pub.aln > mpi_dogs_with_pub_tree.nwk
+FastTree -nt < combined_with_pub.aln > "$OUTDIR/mpi_dogs_with_pub_tree.nwk"
 
-echo "Phylogenetic tree for MPI dogs and published dogs created and saved as 'msa/mpi_dogs_with_pub_tree.nwk'."
+echo "Phylogenetic tree for MPI dogs and published dogs created and saved as '$OUTDIR/mpi_dogs_with_pub_tree.nwk'."
 
 # Create phylogenetic tree with MPI dogs (trim)
-FastTree -nt < combined_trim.aln > mpi_dogs_tree_trim.nwk
+FastTree -nt < combined_trim.aln > "$OUTDIR/mpi_dogs_tree_trim.nwk"
 
-echo "Phylogenetic tree for MPI dogs created and saved as 'msa/mpi_dogs_tree_trim.nwk'."
+echo "Phylogenetic tree for MPI dogs created and saved as '$OUTDIR/mpi_dogs_tree_trim.nwk'."
 
 # Create phylogenetic tree with MPI dogs and published dogs
-FastTree -nt < combined_trim_with_pub.aln > mpi_dogs_with_pub_tree_trim.nwk
+FastTree -nt < combined_trim_with_pub.aln > "$OUTDIR/mpi_dogs_with_pub_tree_trim.nwk"
 
-echo "Phylogenetic tree for MPI dogs and published dogs created and saved as 'msa/mpi_dogs_with_pub_tree_trim.nwk'."
+echo "Phylogenetic tree for MPI dogs and published dogs created and saved as '$OUTDIR/mpi_dogs_with_pub_tree_trim.nwk'."
 
 # Create phylogenetic tree with MPI dogs (cutoff)
-FastTree -nt < combined_cutoff.aln > mpi_dogs_tree_cutoff.nwk
+FastTree -nt < combined_cutoff.aln > "$OUTDIR/mpi_dogs_tree_cutoff.nwk"
 
-echo "Phylogenetic tree for MPI dogs created and saved as 'msa/mpi_dogs_tree_cutoff.nwk'."
+echo "Phylogenetic tree for MPI dogs created and saved as '$OUTDIR/mpi_dogs_tree_cutoff.nwk'."
 
 # Create phylogenetic tree with MPI dogs and published dogs (cutoff)
-FastTree -nt < combined_cutoff_with_pub.aln > mpi_dogs_with_pub_tree_cutoff.nwk
+FastTree -nt < combined_cutoff_with_pub.aln > "$OUTDIR/mpi_dogs_with_pub_tree_cutoff.nwk"
 
-echo "Phylogenetic tree for MPI dogs and published dogs created and saved as 'msa/mpi_dogs_with_pub_tree_cutoff.nwk'."
+echo "Phylogenetic tree for MPI dogs and published dogs created and saved as '$OUTDIR/mpi_dogs_with_pub_tree_cutoff.nwk'."
