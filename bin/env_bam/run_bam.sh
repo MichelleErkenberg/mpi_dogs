cdc1493a3369f7886dbde9547adc1416de951423
Michelle Erkenberg <erkenberg.michelle@gmail.com>
2024-12-06 13:06:16 +0100

    updated script to get figure out the number of all diagnostic positions for each dog of office 1

--- bin/env_bam/run_bam.sh
+++ bin/env_bam/run_bam.sh
@@ -3,22 +3,17 @@
 #creating and define new env_bam file as csv output file
 mkdir -p "$BASE_PATH/data/dog_samples/env_bam"
 env_bam="$BASE_PATH/data/dog_samples/env_bam"
+bam_file="$BASE_PATH/data/env_samples/Canidae/"
+office_file="$BASE_PATH/data/dog_samples/ref/"
 
 #creating *.bam.bai data for every bam to process
-for bam in "$BASE_PATH/data/env_samples/Canidae/*.bam"
-do
-	if [ ! -f "${bam}.bai" ]; then
-        echo "Indexing $bam..."
-        samtools index "$bam"
-	else
-        echo "Index for $bam already exists, skipping."
-    	fi
-done
-
-echo "BAM indexing process completed. All BAM files are now indexed or were already indexed."
+bash bam_to_bai.sh
 
 #creating seperated file for our dogs to structure
-mkdir -p "$env_bam/Heidi"
+#mkdir -p "$env_bam/Heidi"
 
 #finding 
-python3 bam_finder.py "$BASE_PATH/data/dog_samples/ref/office_1/5dogs.Heidi.csv" "$BASE_PATH/data/env_samples/Canidae" "$env_bam/Heidi" "Heidi"
+python3 env_bam/bam_finder_new.py "$office_file/office_1/5dogs.Heidi.csv" "$bam_file" "$env_bam/all_env_Heidi.csv" "Heidi"
+python3 env_bam/bam_finder_new.py "$office_file/office_1/5dogs.Fritzy.csv" "$bam_file" "$env_bam/all_env_Fritzy.csv" "Fritzy"
+python3 env_bam/bam_finder_new.py "$office_file/office_1/5dogs.Vito.csv" "$bam_file" "$env_bam/all_env_Vito.csv" "Vito"
+python3 env_bam/bam_finder_new.py "$office_file/office_1/5dogs.Urza.csv" "$bam_file" "$env_bam/all_env_Urza.csv" "Urza"
