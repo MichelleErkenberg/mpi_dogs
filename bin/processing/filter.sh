1c22b4b7f867c29508ebf3190a3cfc9c53a01110
Michelle Erkenberg <erkenberg.michelle@gmail.com>
2025-03-21 11:34:33 +0200

    changes for filter.sh

--- bin/processing/filter.sh
+++ bin/processing/filter.sh
@@ -1,41 +1,36 @@
 #!/bin/bash
 
-#change the bam file to you bam dir
+# Pfade
 bam_dir="$BASE_PATH/data/dog_samples/processing/bam_files"
 chrM_dir="$BASE_PATH/data/dog_samples/processing/ChrM"
-
-# Change to the working directory
-cd "$BASE_PATH/data/dog_samples" || exit 1
-
 
 ### Step 1: ChrM Extraction ###
 mkdir -p "$chrM_dir"
 echo "Starting ChrM extraction..."
-for bam_file in "$bam_dir/*.bam"; do          
+for bam_file in $bam_dir/*.bam; do          
   base_name=$(basename "$bam_file" .bam)
   samtools view -b "$bam_file" "chrM" > "$chrM_dir/${base_name}_ChrM.bam"
-  echo "Processed: $bam_file → "$chrM_dir/${base_name}_ChrM.bam""
+  echo "Processed: $bam_file → $chrM_dir/${base_name}_ChrM.bam"
 done
-
 
 ### Step 2: MAPQ25 Filtering ###
 mkdir -p "$chrM_dir/MQ25"
 echo -e "\nStarting MAPQ25 filtering..."
-for bam_file in "$chrM_dir/*.bam"; do
+for bam_file in $chrM_dir/*.bam; do
   filename=$(basename "$bam_file" .bam)
   samtools view -bq 25 "$bam_file" > "$chrM_dir/MQ25/${filename}_MQ25.bam"
   samtools index "$chrM_dir/MQ25/${filename}_MQ25.bam"
-  echo "Processed: $bam_file → "$chrM_dir/MQ25/${filename}_MQ25.bam""
+  echo "Processed: $bam_file → $chrM_dir/MQ25/${filename}_MQ25.bam"
 done
 
-
-### Step 3: Deduplication ###
-mkdir -p "$chrM_dir/ChrM/MQ25/dedup"
+### Step 3: Deduplication (modernere Methode) ###
+mkdir -p "$chrM_dir/MQ25/dedup"
 echo -e "\nStarting deduplication..."
-for bam_file in "$chrM_dir/MQ25/*.bam"; do
+for bam_file in $chrM_dir/MQ25/*.bam; do
   filename=$(basename "$bam_file" .bam)
-  samtools rmdup "$bam_file" "$chrM_dir/MQ25/dedup/${filename}_dedup.bam"
+  samtools sort -n "$bam_file" | \
+  samtools fixmate -m - - | \
+  samtools markdup - "$chrM_dir/MQ25/dedup/${filename}_dedup.bam"
   samtools index "$chrM_dir/MQ25/dedup/${filename}_dedup.bam"
-  echo "Deduplicated: $bam_file → "$chrM_dir/MQ25/dedup/${filename}_dedup.bam""
+  echo "Deduplicated: $bam_file → $chrM_dir/MQ25/dedup/${filename}_dedup.bam"
 done
-
