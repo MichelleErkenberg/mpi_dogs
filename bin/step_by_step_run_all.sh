a9b1c30649fe9495a455f3bf0cf259bb331a791b
Michelle Erkenberg <erkenberg.michelle@gmail.com>
2025-03-25 10:03:23 +0200

    changes in msa script

--- bin/step_by_step_run_all.sh
+++ bin/step_by_step_run_all.sh
@@ -10,8 +10,6 @@ elif [[ "$b" == "y" ]]; then
 	echo "BASE_PATH changed to "$bn""	
 fi
 
-cd /bin
-echo "changed to /bin"
 
 while true; do
 
@@ -21,58 +19,58 @@ read -p "enter the process to be executed (press Enter for showing avalible proc
 #processing the data, filtering for ChrM and Quality and counting
 if [[ "$x" == "processing" ]]; then
 	#filter for the mitochondrial Chromosom, mapquaility 25% and dedup
-	bash processing/filter.sh
+	bash bin/processing/filter.sh
 	echo "data processed"
 
 	
 elif [[ "$x" == "count" ]]; then	
 	#count the sequences and for orignial data count also for sequences per chromosom
-	bash processing/sequence_counter.sh
+	bash bin/processing/sequence_counter.sh
 	echo "amount of sequences pre chromosom and for ChrM counted"
 	
 #Call a consensus sequence for each dog using Matthias' perl script
 elif [[ "$x" == "consensus" ]]; then
-	bash consensus/consensus.sh	
+	bash bin/consensus/consensus.sh	
 	echo "consensus sequences created"
 
 #msa for the created consensus sequences (renaming the sequence as part of the masking process)
 elif [[ "$x" == "msa" ]]; then
 	#copies and renames the consensus sequences for all of our dogs, Undetermined is deleted in the process
-	bash msa/mask.sh
+	bash bin/msa/mask.sh
 	echo "masking finished"
 
 	#cutoff the sequence of all our dogs after TTTTAGG/AAG
-	bash msa/cutoff_seq.sh
+	bash bin/msa/cutoff_seq.sh
 	echo "sequeces were cut after TTTTAGG/AAG"
 	
 	#all of your dogs + previously published ones + reference dog combined in one script each + msa
-	bash msa/msa.sh 
+	bash bin/msa/msa.sh 
 	echo "MSA created"
 
 #create phylogenetic tree for our dogs and the published dogs with a reference one
 elif [[ "$x" == "tree" ]]; then
 
-	bash tree/tree.sh
+	bash bin/tree/tree.sh
 	echo "phylogenentic tree created"
 
 #finding differences and replace the n's (also see diff.sh for more details); it takes the latest aln so far and trims it to 16138bp; afterwards closely related dog sequences are extracted (manually) and differences are highlighted in an csv file; the n's from our dogs are than replaced (if possible) with the bases from the replated dogs; that leads to an csv file with replaced n's and in the long term to an fasta file with out dogs + replaced possions 
 elif [[ "$x" == "differences" ]]; then
-	bash diff/diff.sh
+	bash bin/diff/diff.sh
 	echo "differences found, N's replaced"
 
 #using the ref dog genome for genome coordinates
 elif [[ "$x" == "coordinates" ]]; then
 
 	mkdir -p "$BASE_PATH/data/dog_samples/ref"
-	python3 ref/ref_coor.py "$BASE_PATH/data/dog_samples/diff/replaced_seq.related_n.mpi_dogs.added_ref.aln" "$BASE_PATH/data/dog_samples/ref/ref_coordinates.csv" NC_002008.4 fasta
+	python3 bin/ref/ref_coor.py "$BASE_PATH/data/dog_samples/diff/replaced_seq.related_n.mpi_dogs.added_ref.aln" "$BASE_PATH/data/dog_samples/ref/ref_coordinates.csv" NC_002008.4 fasta
 	echo "genomic coordinates detected"
 
 #dogs were living in different offices, this script creates files with just the dogs that lived together 
 elif [[ "$x" == "snps" ]]; then
-	bash ref/run_extract_dog.sh
+	bash bin/ref/run_extract_dog.sh
 
 #finding the private position for our dogs in the environment data
-	bash env_bam/run_bam.sh
+	bash bin/env_bam/run_bam.sh
 	echo "private positions for each dog detected and compared with environmental data" 
 
 #prepairing the environmental dog data to process them using R 
@@ -87,7 +85,7 @@ elif [[ "$x" == "R" ]]; then
 #python3 R_prep/csv_prep.py "$BASE_PATH/data/dog_samples/env_bam/all_env_AC*.csv" "$BASE_PATH/data/dog_samples/R_prep/all_dogs_AC/R_prep_sample_vs_dog_AC.csv" "2"
 #creates csv file with average radio for each dog by using all dog (except Thor A and B and Lily)
 	mkdir -p "$BASE_PATH/data/dog_samples/R_prep/all_dogs_ACwoL"
-	python3 R_prep/csv_prep.py "$BASE_PATH/data/dog_samples/env_bam/all_env_ACwoL/all_env_AC*.csv" "$BASE_PATH/data/dog_samples/R_prep/all_dogs_ACwoL/R_prep_sample_vs_dog_ACwoL_2snp.csv" "$s"
+	python3 bin/R_prep/csv_prep.py "$BASE_PATH/data/dog_samples/env_bam/all_env_ACwoL/all_env_AC*.csv" "$BASE_PATH/data/dog_samples/R_prep/all_dogs_ACwoL/R_prep_sample_vs_dog_ACwoL_2snp.csv" "$s"
 	echo "environmental data processed with "$s" SNPs"
 
 #uses the txt file with the location to sort those average radios into new csv files
@@ -96,7 +94,7 @@ elif [[ "$x" == "R" ]]; then
 
 # for running the entire script
 elif [[ "$x" == "all" ]]; then
-	bash run_all.sh
+	bash bin/run_all.sh
 
 else
 	echo "avalible processes:"
