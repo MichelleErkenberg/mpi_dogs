0fffba8a65206592540c6fb95a6e665acc8e196f
Michelle Erkenberg <erkenberg.michelle@gmail.com>
2025-03-25 14:48:04 +0200

    changes in diff related scripts

--- bin/env_bam/run_bam.sh
+++ bin/env_bam/run_bam.sh
@@ -1,47 +1,88 @@
 #!/bin/bash
 
-#creating and define new env_bam file as csv output file
+source ./bin/ref/run_extrac_dog.sh
+
+# Creating and defining new env_bam file as csv output file
 mkdir -p "$BASE_PATH/data/dog_samples/env_bam"
 env_bam="$BASE_PATH/data/dog_samples/env_bam"
 export bam_file="$BASE_PATH/data/env_samples/quicksand.v2/out/Canidae/fixed/3-deduped/"
 office_file="$BASE_PATH/data/dog_samples/ref/"
 
-#creating *.bam.bai data for every bam to process
-#bash env_bam/bam_to_bai.sh
+# Creating *.bam.bai data for every bam to process
+# bash env_bam/bam_to_bai.sh
 
+while true; do
+    read -p "Only compare offices or excluded files (raw/excluded)?: " e 
+    if [[ "$e" == "raw" ]]; then
+        # All dogs office files
+        declare -A dogs=(
+            ["Heidi"]="$office_file/office_container/5dogs.Heidi.csv"
+            ["Fritzy"]="$office_file/office_container/5dogs.Fritzy.csv"
+            ["Vito"]="$office_file/office_container/5dogs.Vito.csv"
+            ["Urza"]="$office_file/office_container/5dogs.Urza.csv"
+            ["Cami"]="$office_file/office_container/5dogs.Cami.csv"
+            ["Lily"]="$office_file/office_thorA.lily/3dogs.Lily.csv"
+            ["ThorA"]="$office_file/office_thorA.lily/3dogs.ThorA.csv" 
+        )
 
-#finding 
-python3 env_bam/bam_finder_new.py "$office_file/office_container/5dogs.Heidi.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_Heidi.csv" "Heidi"
-python3 env_bam/bam_finder_new.py "$office_file/office_container/5dogs.Fritzy.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_Fritzy.csv" "Fritzy"
-python3 env_bam/bam_finder_new.py "$office_file/office_container/5dogs.Vito.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_Vito.csv" "Vito"
-python3 env_bam/bam_finder_new.py "$office_file/office_container/5dogs.Urza.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_Urza.csv" "Urza"
-python3 env_bam/bam_finder_new.py "$office_file/office_container/5dogs.Cami.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_Cami.csv" "Cami"
+        # Loop for all dogs
+        for dog in "${!dogs[@]}"; do
+            input_file="${dogs[$dog]}"
+            output_file="$env_bam/all_env_${dog}.csv"
+            python3 env_bam/bam_finder_new.py "$input_file" "$office_file/ref_coordinates.csv" "$bam_file" "$output_file" "$dog"
+        done
 
-python3 env_bam/bam_finder_new.py "$office_file/office_thorA.lily/3dogs.Lily.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_Lily.csv" "Lily"
-python3 env_bam/bam_finder_new.py "$office_file/office_thorA.lily/3dogs.ThorA.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_ThorA.csv" "ThorA"
+    elif [[ "$e" == "exclude" ]]; then
+        # All excluded files
+        declare -A dogs_ex=(
+            ["$a"]="$office_file/all_dogs_with_${a}_${b}/all_dogs_${a}${b}.${a}.csv"
+            ["$b"]="$office_file/all_dogs_with_${a}_${b}/all_dogs_${a}${b}.${b}.csv" 
+            ["Heidi"]="$office_file/all_dogs_with_${a}_${b}/all_dogs_${a}${b}.Heidi.csv" 
+            ["Fritzy"]="$office_file/all_dogs_with_${a}_${b}/all_dogs_${a}${b}.Fritzy.csv"
+            ["Vito"]="$office_file/all_dogs_with_${a}_${b}/all_dogs_${a}${b}.Vito.csv" 
+            ["Urza"]="$office_file/all_dogs_with_${a}_${b}/all_dogs_${a}${b}.Urza.csv" 
+            ["Lily"]="$office_file/all_dogs_with_${a}_${b}/all_dogs_${a}${b}.Lily.csv" 
+            ["Charlie"]="$office_file/all_dogs_with_${a}_${b}/all_dogs_${a}${b}.Charlie.csv" 
+        )
 
+        mkdir -p "$env_bam/all_dogs_with_${a}_${b}"
+        # Loop for all dogs
+        for dog_ex in "${!dogs_ex[@]}"; do
+            input_file="${dogs_ex[$dog_ex]}"
+            output_file="$env_bam/all_dogs_with_${a}_${b}/all_env_${dog_ex}.csv"
+            python3 env_bam/bam_finder_new.py "$input_file" "$office_file/ref_coordinates.csv" "$bam_file" "$output_file" "$dog_ex"
+        done
 
-python3 env_bam/bam_finder_new.py "$office_file/office_anda.charlie/3dogs.Anda.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_Anda.csv" "Anda"
-python3 env_bam/bam_finder_new.py "$office_file/office_anda.charlie/3dogs.Charlie.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_Charlie.csv" "Charlie"
+        # Lily
+        read -p "Also process data without Lily (n/y)?: " l
+        if [[ "$l" == "y" ]]; then
+            mkdir -p "$env_bam/all_dogs_with_${a}_${b}_without_Lily"
 
-python3 env_bam/bam_finder_new.py "$office_file/office_anda.charlie/3dogs.Anda_Vito.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_Anda_Vito.csv" "Anda"
+            # All excluded files without lily
+            declare -A dogs_exl=(
+                ["$a"]="$office_file/all_dogs_with_${a}_${b}_without_Lily/all_dogs_${a}${b}woL.${a}.csv"
+                ["$b"]="$office_file/all_dogs_with_${a}_${b}_without_Lily/all_dogs_${a}${b}woL.${b}.csv" 
+                ["Heidi"]="$office_file/all_dogs_with_${a}_${b}_without_Lily/all_dogs_${a}${b}woL.Heidi.csv" 
+                ["Fritzy"]="$office_file/all_dogs_with_${a}_${b}_without_Lily/all_dogs_${a}${b}woL.Fritzy.csv"
+                ["Vito"]="$office_file/all_dogs_with_${a}_${b}_without_Lily/all_dogs_${a}${b}woL.Vito.csv" 
+                ["Urza"]="$office_file/all_dogs_with_${a}_${b}_without_Lily/all_dogs_${a}${b}woL.Urza.csv" 
+                ["Charlie"]="$office_file/all_dogs_with_${a}_${b}_without_Lily/all_dogs_${a}${b}woL.Charlie.csv" 
+            )
 
-python3 env_bam/bam_finder_new.py "$office_file/all_dogs/all_dogs_AC.Anda.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_AC.Anda.csv" "Anda"
-python3 env_bam/bam_finder_new.py "$office_file/all_dogs/all_dogs_AC.Cami.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_AC.Cami.csv" "Cami"
-python3 env_bam/bam_finder_new.py "$office_file/all_dogs/all_dogs_AC.Charlie.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_AC.Charlie.csv" "Charlie"
-python3 env_bam/bam_finder_new.py "$office_file/all_dogs/all_dogs_AC.Fritzy.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_AC.Fritzy.csv" "Fritzy"
-python3 env_bam/bam_finder_new.py "$office_file/all_dogs/all_dogs_AC.Heidi.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_AC.Heidi.csv" "Heidi"
-python3 env_bam/bam_finder_new.py "$office_file/all_dogs/all_dogs_AC.Lily.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_AC.Lily.csv" "Lily"
-python3 env_bam/bam_finder_new.py "$office_file/all_dogs/all_dogs_AC.Urza.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_AC.Urza.csv" "Urza"
-python3 env_bam/bam_finder_new.py "$office_file/all_dogs/all_dogs_AC.Vito.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_AC.Vito.csv" "Vito"
+            # Loop for all dogs without lily
+            for dog_exl in "${!dogs_exl[@]}"; do
+                input_file="${dogs_exl[$dog_exl]}"
+                output_file="$env_bam/all_dogs_with_${a}_${b}_without_Lily/all_env_${dog_exl}.csv"
+                python3 env_bam/bam_finder_new.py "$input_file" "$office_file/ref_coordinates.csv" "$bam_file" "$output_file" "$dog_exl"
+            done
+        fi
+    else
+        echo "Invalid option. Please choose 'raw' or 'exclude'."
+        continue
+    fi
 
-
-mkdir -p "$env_bam/all_env_ACwoL"
-python3 env_bam/bam_finder_new.py "$office_file/all_dogs_AC_without_Lily/all_dogs_ACwoL.Anda.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_ACwoL/all_env_ACwoL.Anda.csv" "Anda"
-python3 env_bam/bam_finder_new.py "$office_file/all_dogs_AC_without_Lily/all_dogs_ACwoL.Cami.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_ACwoL/all_env_ACwoL.Cami.csv" "Cami"
-python3 env_bam/bam_finder_new.py "$office_file/all_dogs_AC_without_Lily/all_dogs_ACwoL.Charlie.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_ACwoL/all_env_ACwoL.Charlie.csv" "Charlie"
-python3 env_bam/bam_finder_new.py "$office_file/all_dogs_AC_without_Lily/all_dogs_ACwoL.Fritzy.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_ACwoL/all_env_ACwoL.Fritzy.csv" "Fritzy"
-python3 env_bam/bam_finder_new.py "$office_file/all_dogs_AC_without_Lily/all_dogs_ACwoL.Heidi.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_ACwoL/all_env_ACwoL.Heidi.csv" "Heidi"
-python3 env_bam/bam_finder_new.py "$office_file/all_dogs_AC_without_Lily/all_dogs_ACwoL.Urza.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_ACwoL/all_env_ACwoL.Urza.csv" "Urza"
-python3 env_bam/bam_finder_new.py "$office_file/all_dogs_AC_without_Lily/all_dogs_ACwoL.Vito.csv" "$office_file/ref_coordinates.csv" "$bam_file" "$env_bam/all_env_ACwoL/all_env_ACwoL.Vito.csv" "Vito"
-
+    read -p "Continue searching for SNPs (y/n)?: " s
+    if [[ "$s" != "y" ]]; then
+        break
+    fi
+done
